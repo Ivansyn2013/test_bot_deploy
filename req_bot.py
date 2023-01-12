bfe8a901b81bc2bd130f3b5ec866ab8c086b534c
ivan <ivansyn2013@yandex.ru>
2023-01-12 23:31:01 +0300

    change req

--- req_bot.py
+++ req_bot.py
@@ -1,39 +1,46 @@
 import requests
 from dotenv import load_dotenv
 import os
+import fire
 
-load_dotenv()
-BOTTOKEN = os.getenv('BOTTOKEN')
-def registation():
-    #нужно передать самоподписной сертификат
-    req = requests.post(f'https://api.telegram.org/bot{BOTTOKEN}/setWebhook',
-                        headers={'Content-Type': 'application/json'},
-                        json={'url':'https://176.119.157.117'},
-                        certificate=)
+class Webhooks_setttings():
+    load_dotenv()
+    BOTTOKEN = os.getenv('BOTTOKEN')
+    def registation(self):
+        #нужно передать самоподписной сертификат
+        try:
+            file = open('/etc/ssl/certs/nginx_test.pem', 'rb')
+        except:
+            return None
+        req = requests.post(f'https://api.telegram.org/bot{self.BOTTOKEN}/setWebhook',
+                            headers={'Content-Type': 'application/json'},
+                            json={'url':'https://176.119.157.117'},
+                            certificate=file,
+                            )
 
 
-    print(req.json)
-    print(req.headers)
-    print(req.text)
+        print(req.json)
+        print(req.headers)
+        print(req.text)
 
-def get_webhook_info():
-    req = requests.post(f'https://api.telegram.org/bot{BOTTOKEN}/getWebhookInfo',
-                        headers={'Content-Type': 'application/json'},
-                        )
+    def get_webhook_info(self):
+        req = requests.post(f'https://api.telegram.org/bot{self.BOTTOKEN}/getWebhookInfo',
+                            headers={'Content-Type': 'application/json'},
+                            )
 
-    print(req.json)
-    print(req.headers)
-    print(req.text)
+        print(req.json)
+        print(req.headers)
+        print(req.text)
 
-def del_webhook_():
-    req = requests.post(f'https://api.telegram.org/bot{BOTTOKEN}/getWebhookInfo',
-                        headers={'Content-Type': 'application/json'},
-                        )
+    def del_webhook(self):
+        req = requests.post(f'https://api.telegram.org/bot{self.BOTTOKEN}/deleteWebhook',
+                            headers={'Content-Type': 'application/json'},
+                            )
 
-    print(req.json)
-    print(req.headers)
-    print(req.text)
+        print(req.json)
+        print(req.headers)
+        print(req.text)
 
-get_webhook_info()
 
-get_webhook_info()
+if __name__=='__main__':
+    fire.Fire(Webhooks_setttings)
