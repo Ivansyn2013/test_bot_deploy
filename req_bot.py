0a8d2f10db2bb73f1ebf72350c9727d4acf2d4b6
ivan <ivansyn2013@yandex.ru>
2023-01-12 23:04:52 +0300

    some changes

--- req_bot.py
+++ req_bot.py
@@ -4,11 +4,36 @@ import os
 
 load_dotenv()
 BOTTOKEN = os.getenv('BOTTOKEN')
-req = requests.post(f'https://api.telegram.org/bot{BOTTOKEN}/setWebhook',
-                    headers={'Content-Type': 'application/json'},
-                    json={'url':'https://176.119.157.117'})
+def registation():
+    #нужно передать самоподписной сертификат
+    req = requests.post(f'https://api.telegram.org/bot{BOTTOKEN}/setWebhook',
+                        headers={'Content-Type': 'application/json'},
+                        json={'url':'https://176.119.157.117'},
+                        certificate=)
 
 
-print(req.json)
-print(req.headers)
-print(req.text)
+    print(req.json)
+    print(req.headers)
+    print(req.text)
+
+def get_webhook_info():
+    req = requests.post(f'https://api.telegram.org/bot{BOTTOKEN}/getWebhookInfo',
+                        headers={'Content-Type': 'application/json'},
+                        )
+
+    print(req.json)
+    print(req.headers)
+    print(req.text)
+
+def del_webhook_():
+    req = requests.post(f'https://api.telegram.org/bot{BOTTOKEN}/getWebhookInfo',
+                        headers={'Content-Type': 'application/json'},
+                        )
+
+    print(req.json)
+    print(req.headers)
+    print(req.text)
+
+get_webhook_info()
+
+get_webhook_info()
